dd864b493f8ca6d0d4aa99c460c67d5411be666b
Junaid Mohammed <96166952+Junaid-KinT@users.noreply.github.com>
2025-02-12 13:09:46 +0530

    Add files via upload

---
+++ devportal/src/main/java/com/concerto/devportal/ConcertoDevPortalApplication.java
@@ -0,0 +1,13 @@
+package com.concerto.devportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcertoDevPortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConcertoDevPortalApplication.class, args);
+	}
+
+}
